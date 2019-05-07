2dfb4db4d701bcce26e8c6aa4f15e24a59db0fcb
Gian Libunao <gianlibunao@gmail.com>
2019-05-07 03:44:33 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,6 +2,7 @@ require "pry"
 def reformat_languages(languages)
   # your code here
   new_hash = {}
+<<<<<<< HEAD
   top_level(languages).each{|x,y| new_hash[x]=y.merge(style:[])}
   #new_hash.keys.each{|x| add_style(x,languages,new_hash)}
   new_hash.keys.each{|key| 
@@ -15,6 +16,10 @@ def reformat_languages(languages)
 }
   #binding.pry
   new_hash
+=======
+  top_level(languages).each{|x,y| new_hash[x]=y}
+  binding.pry
+>>>>>>> ea269e278ab21ad673f10de4fa7cc5ce80725b0b
 end
 
 def top_level(languages)
@@ -22,8 +27,12 @@ def top_level(languages)
   languages.keys.each{|x| retHash=retHash.merge(languages[x])
   }
   retHash
+<<<<<<< HEAD
 end
 
 #def add_style(pain,languages,new_hash)
 #  languages.each{|x,y| y.each{|x1,y1| if x1==pain new_hash[pain][:style] << x end }}
 #end
+=======
+end
+>>>>>>> ea269e278ab21ad673f10de4fa7cc5ce80725b0b
